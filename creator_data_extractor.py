8cd366647b72e73fa70a8b92e3060491a41f4bd3
Jaber Chowdhury <jaberhaisan@gmail.com>
2023-06-09 22:05:14 -0600

    Added created page scraping

--- creator_data_extractor.py
+++ creator_data_extractor.py
@@ -68,57 +68,6 @@ def extract_elem_text(soup, selector):
     else:
         return elem.getText()
 
-def extract_creator_data(path, is_link=True):
-    """Returns a dictionary of the data for the creator."""
-    data = {}
-
-    if is_link:
-        # Extract data from about page.
-        about_soup = get_live_soup(path + "/about")
-        comment_soup = get_live_soup(path + "/comments", True)
-    else:
-        with open(path, encoding='utf8', errors="backslashreplace") as infile:
-            about_soup = BeautifulSoup(infile, "lxml")        
-
-    # Number of projects backed.
-    backed = extract_elem_text(about_soup, 'span[class="backed"]')
-    data['num_backed'] = get_digits(backed, "int")
-    
-    # Join date.
-    join_day, join_month, join_year = "", "", ""
-    join_date_elem = about_soup.select_one('span[class="joined"] > time')
-    if join_date_elem != None:
-        join_date = datetime.strptime(join_date_elem['datetime'], '%Y-%m-%dT%H:%M:%S%z')
-        join_day, join_month, join_year = join_date.day, join_date.month, join_date.year
-        
-    data['join_day'], data['join_month'], data['join_year'] = join_day, join_month, join_year
-
-    # Location.
-    data['location'] = extract_elem_text(about_soup, 'span[class="location"] > a')
-
-    # Biography.
-    data['biography'] = extract_elem_text(about_soup, 'div[class="grid-col-12 grid-col-8-sm grid-col-6-md"]').strip()
-
-    # Number of created projects.
-    data['num_created'] = extract_elem_text(about_soup, 'a[class="nav--subnav__item__link nav--subnav__item__link--gray js-created-link"] > span').strip()
-
-    # Websites.
-    data['websites'] = [elem['href'] for elem in about_soup.select('ul[class="menu-submenu mb6"] > li > a')]
-
-    # Comments.
-    comments = []
-    comment_elems = comment_soup.select('li[class="flex flex-wrap page"] > ol > li')
-    for comment_elem in comment_elems:
-        comment_str = comment_elem.select_one('p[class="body"]').getText()
-        link = "https://www.kickstarter.com" + comment_elem.select_one('a[class="read-more"]')['href']
-        date = comment_elem.select_one('a[class="read-more"] > time').getText()
-        comments.append((comment_str, date, link))
-
-    data['num_comments'] = len(comments)
-    data['comments'] = comments
-
-    return data
-
 def parse_data_project(data_project):
     """Parses a kickstarter data project dictionary and returns a dictionary of
     required keys.
@@ -151,21 +100,85 @@ def parse_data_project(data_project):
 
     return result
 
+def extract_creator_data(path, is_link=True):
+    """Returns a dictionary of the data for the creator."""
+    data = {}
+
+    if is_link:
+        # Extract data from about page.
+        about_soup = get_live_soup(path + "/about")
+        created_soup = get_live_soup(path + "/created", True)
+        comment_soup = get_live_soup(path + "/comments", True)
+    else:
+        with open(path + " — About.html", encoding='utf8', errors="backslashreplace") as infile:
+            about_soup = BeautifulSoup(infile, "lxml")
+        with open(path + " — Comments.html", encoding='utf8', errors="backslashreplace") as infile:
+            comment_soup = BeautifulSoup(infile, "lxml")
+        with open(path + " — Created.html", encoding='utf8', errors="backslashreplace") as infile:
+            created_soup = BeautifulSoup(infile, "lxml")         
+
+    # Creator id.
+    url_elem = about_soup.select_one('meta[property="og:url"]')
+    data["url"] = url_elem["content"]
+
+    # Project Id and Creator Id.
+    data["creator_id"] = data["url"].split("/")[-1]
+
+    # Join date.
+    join_day, join_month, join_year = "", "", ""
+    join_date_elem = about_soup.select_one('span[class="joined"] > time')
+    if join_date_elem != None:
+        join_date = datetime.strptime(join_date_elem['datetime'], '%Y-%m-%dT%H:%M:%S%z')
+        join_day, join_month, join_year = join_date.day, join_date.month, join_date.year
+        
+    data['join_day'], data['join_month'], data['join_year'] = join_day, join_month, join_year
+
+    # Location.
+    data['location'] = extract_elem_text(about_soup, 'span[class="location"] > a')
+
+    # Biography.
+    data['biography'] = extract_elem_text(about_soup, 'div[class="grid-col-12 grid-col-8-sm grid-col-6-md"]').strip()
+
+    # Number of projects backed.
+    backed = extract_elem_text(about_soup, 'span[class="backed"]')
+    data['num_backed'] = get_digits(backed, "int")
+
+    # Number of created projects.
+    data['num_created'] = extract_elem_text(about_soup, 'a[class="nav--subnav__item__link nav--subnav__item__link--gray js-created-link"] > span').strip()
+
+    # Websites.
+    data['websites'] = [elem['href'] for elem in about_soup.select('ul[class="menu-submenu mb6"] > li > a')]
+
+    # Comments.
+    comments = []
+    comment_elems = comment_soup.select('li[class="flex flex-wrap page"] > ol > li')
+    for comment_elem in comment_elems:
+        comment_str = comment_elem.select_one('p[class="body"]').getText()
+        link = "https://www.kickstarter.com" + comment_elem.select_one('a[class="read-more"]')['href']
+        date = comment_elem.select_one('a[class="read-more"] > time').getText()
+        comments.append((comment_str, date, link))
+
+    data['num_comments'] = len(comments)
+    data['comments'] = comments
+
+    # Created projects.
+    created_project_elem = created_soup.select_one('div[data-projects]')
+    created_data_projects = json.loads(created_project_elem['data-projects'])
+
+    created_projects = []
+    for created_data_project in created_data_projects:
+        created_projects.append(parse_data_project(created_data_project))
+    data['created_projects'] = created_projects
+
+    return data
+
 if __name__ == "__main__":
     # https://www.kickstarter.com/profile/dicedungeons/comments # Lots of loading comments.
     # https://www.kickstarter.com/profile/shiftcam # Backed projects are public.
     # https://www.kickstarter.com/profile/mybirdbuddy/about # Multiple websites in about.
 
-    # data = extract_creator_data(r"C:\Users\jaber\OneDrive\Desktop\Research_JaberChowdhury\Kickstarter-Data-Scraper\Test\Creator\Soul Mama London — About.html", False)
-    # df = pd.DataFrame([data])
-    # df.to_csv('creator_test.csv', index = False)
-    path = r"C:\Users\jaber\OneDrive\Desktop\Research_JaberChowdhury\Kickstarter-Data-Scraper\Test\Creator\Dice Dungeons — Created.html"
+    data = extract_creator_data(r"C:\Users\jaber\OneDrive\Desktop\Research_JaberChowdhury\Kickstarter-Data-Scraper\Test\Creator\Dice Dungeons", False)
+    df = pd.DataFrame([data])
+    df.to_csv('creator_test.csv', index = False)
 
-    with open(path, encoding='utf8', errors="backslashreplace") as infile:
-        created_soup = BeautifulSoup(infile, "lxml")
-
-    data_project = created_soup.select_one('div[data-projects]')['data-projects']
-    d = json.loads(data_project)[0]
-    parse_data_project(d)
      
-
