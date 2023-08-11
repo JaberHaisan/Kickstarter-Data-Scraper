7f6002859256b7d9d10d6bad935d85b8fde10fd8
Jaber Chowdhury <jaberhaisan@gmail.com>
2023-08-11 10:28:31 +0600

    Suppressed unnecessary logging messages from uc

--- extra_project_finder.py
+++ extra_project_finder.py
@@ -16,7 +16,7 @@ from selenium.webdriver.common.by import By
 import pyautogui
 from bs4 import BeautifulSoup
 
-# Location of new_creator_ids.json
+# Location of json with creator ids.
 CREATOR_ID_PATH = r'D:\unscraped_creators_0.json'
 # Location of kickstarter_existing_links.json
 EXISTING_LINKS_PATH = r'D:\kickstarter_existing_links.json' 
@@ -24,13 +24,14 @@ EXISTING_LINKS_PATH = r'D:\kickstarter_existing_links.json'
 OUTPUT_PATH = r"D:"
 # Chromedriver path
 CHROMEDRIVER_PATH = r"C:\Users\jaber\OneDrive\Desktop\Research_JaberChowdhury\Kickstarter-Data-Scraper\chromedriver.exe"
+# Proton vpn windows taskbar location.
+icon_num = 5 
 
 # Number of processes per try.
 chunk_size = 10
-# Set logging.
+# Set logging. 
+logging.getLogger('uc').setLevel(logging.ERROR)
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO, datefmt='%m/%d/%Y %I:%M:%S %p')
-# Proton vpn windows taskbar location.
-icon_num = 5 
 # Pyautogui settings.
 pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
@@ -172,7 +173,7 @@ def get_live_soup(link, scroll=False, given_driver=None):
     False by default.
     given_driver [selenium webdriver] - A webdriver. None by default."""
     if given_driver == None:
-        driver = uc.Chrome(executable_path=CHROMEDRIVER_PATH)
+        driver = uc.Chrome(executable_path=CHROMEDRIVER_PATH, headless=True)
     else:
         driver = given_driver
     driver.get(link)
@@ -282,7 +283,7 @@ def extract_creator_data(creator_id):
     logging.info(f"Started extracting {creator_id} data...")
     path = r"https://www.kickstarter.com/profile/" + str(creator_id)
 
-    driver = uc.Chrome(executable_path=CHROMEDRIVER_PATH)
+    driver = uc.Chrome(executable_path=CHROMEDRIVER_PATH, headless=True)
     # Extract data from available pages. There may be multiple pages for created projects.
 
     try:
